6759823debd5de90b9bfaeb6b6a73acc99a2f627
Skeyll <studio.skeyll@gmail.com>
2023-04-11 17:36:09 +0900

    Can't use newline.

--- SynicSugar/Assets/SynicSugar/Editor/PostProcess/ILViewer.cs
+++ SynicSugar/Assets/SynicSugar/Editor/PostProcess/ILViewer.cs
@@ -1,18 +1,14 @@
 using System;
-using System.Collections.Generic;
+using Mono.Collections.Generic;
 using Mono.Cecil.Cil;
 
 namespace SynicSugar.PostProcess {
     public class ILViewer {
-        public string fullIL { get; private set;} = System.String.Empty;
-
-        public void AddIl(List<Instruction> instructions){
+        public string il { get; private set;} = System.String.Empty;
+        public void ExpandIL(Collection<Instruction> instructions){
             foreach(var instruction in instructions){
-                fullIL += (instruction + Environment.NewLine);
+                il += $"{instruction} //";
             }
-        }
-        public void AddIl(Instruction instruction){
-            fullIL += (instruction + Environment.NewLine);
         }
     }
 }
