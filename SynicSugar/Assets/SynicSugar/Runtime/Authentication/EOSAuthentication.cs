cee6b90703315de5df59185273c5054a3d71f8f5
Skeyll <studio.skeyll@gmail.com>
2023-04-13 15:25:34 +0900

    Change: instance is needless.

--- SynicSugar/Assets/SynicSugar/Runtime/Authentication/EOSAuthentication.cs
+++ SynicSugar/Assets/SynicSugar/Runtime/Authentication/EOSAuthentication.cs
@@ -6,43 +6,29 @@ using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 namespace SynicSugar.Auth {
-    public class EOSAuthentication : MonoBehaviour {
-    #region Singleton Instance
-        private EOSAuthentication(){}
-        public static EOSAuthentication Instance { get; private set; }
-        void Awake() {
-            if( Instance != null ) {
-                Destroy( this.gameObject );
-                return;
-            }
-            Instance = this;
-        }
-        void OnDestroy() {
-            if( Instance == this ) {
-                Instance = null;
-            }
-        }
-    #endregion
-        public async UniTask<bool> LoginWithDeviceID(CancellationTokenSource token){
+    public static class EOSAuthentication {
+        public static async UniTask<bool> LoginWithDeviceID(CancellationTokenSource token){
             bool isSuccess = false;
             bool waitingAuth = true;
             //DeviceID
             var connectInterface = EOSManager.Instance.GetEOSPlatformInterface().GetConnectInterface();
             var createDeviceIdOptions = new Epic.OnlineServices.Connect.CreateDeviceIdOptions() { DeviceModel = SystemInfo.deviceModel };
+
             connectInterface.CreateDeviceId(ref createDeviceIdOptions, null, 
                 (ref CreateDeviceIdCallbackInfo data) => {
                     if (data.ResultCode == Result.Success) {
                         isSuccess = true;
-                        Debug.Log("Create new DeviceId");
+                        Debug.Log("EOS AUTH: Create new DeviceId");
                     }else if (data.ResultCode == Result.DuplicateNotAllowed){
                         isSuccess = true;
-                        Debug.Log("Already create DeviceID");
+                        Debug.Log("EOS AUTH: Already create DeviceID");
                     }
                     waitingAuth = false;
                 });
             await UniTask.WaitUntil(() => !waitingAuth, cancellationToken: token.Token);
 
             if(!isSuccess){
+                Debug.Log("EOS AUTH: can't get device id");
                 return false;
             }
             //Login
@@ -50,7 +36,6 @@ namespace SynicSugar.Auth {
             //Pass UserID on each Game.
             EOSManager.Instance.StartConnectLoginWithDeviceToken("UnityEditorLocalUser", info => {
                     Debug.Log(info.ResultCode);
-                    Debug.Log(info.LocalUserId);
                     isSuccess = (info.ResultCode == Result.Success);
                     waitingAuth = false;
                 });
