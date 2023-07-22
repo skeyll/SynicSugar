f0bfb54a58218a0deb7420aef3aab000ba4be618
Skeyll <studio.skeyll@gmail.com>
2023-07-22 14:39:28 +0900

    Add header.

--- SynicSugar/Assets/SynicSugar/Runtime/p2p/EOSp2p.cs
+++ SynicSugar/Assets/SynicSugar/Runtime/p2p/EOSp2p.cs
@@ -70,19 +70,28 @@ namespace SynicSugar.P2P {
         }
         /// <summary>
         /// Send a large packet to a specific peer. To send returner. </ br>
+        /// Add header to sent divided packets.
         /// *Current: We can use this from a specific API.
         /// </summary>
         /// <param name="ch">Only 255 now</param>
         /// <param name="value">The payload serialized with MemoryPack and BrotliCompressor</param>
         /// <param name="targetId"></param>
-        public static void SendLargePacket(byte ch, byte[] value, UserId targetId){
+        /// <param name="hierarchyLevel">Sync from 0 to hierarchy</param>
+        /// <param name="syncAllHierarchy">If false, synchronize an only specific hierarchy</param>
+        public static void SendLargePacket(byte ch, byte[] value, UserId targetId, byte hierarchyLevel = 255, bool syncSpecificHierarchy = false){
             int length = 1100;
+            byte chunkIndex = 0;
+            byte chunkAmount = (byte)Math.Ceiling(value.Length / 1100f);
             //Max payload is 1170 but we need some header.
             for(int startIndex = 0; startIndex < value.Length; startIndex += 1100){
                 length = startIndex + 1100 < value.Length ? 1100 : value.Length - startIndex;
 
                 Span<byte> _payload = new Span<byte>(value, startIndex, length); 
-                byte[] payload = _payload.ToArray();
+                byte[] header = GenerateHeader(chunkAmount, hierarchyLevel, syncSpecificHierarchy, ref chunkIndex);
+                //Add header
+                byte[] payload = new byte[header.Length + length];
+                Array.Copy(header, 0, payload, 0, header.Length);
+                Array.Copy(_payload.ToArray(), 0, payload, header.Length, length);
 
                 SendPacketOptions options = new SendPacketOptions(){
                     LocalUserId = EOSManager.Instance.GetProductUserId(),
@@ -104,6 +113,23 @@ namespace SynicSugar.P2P {
         #if SYNICSUGAR_LOG
             Debug.Log($"Send Large Packet: Success to {targetId.ToString()}!");
         #endif
+            /// <summary>
+            /// index, (chunk count, hierarchy level, only sync hierarchy), tail
+            /// </summary>
+            byte[] GenerateHeader(byte chunk, byte hierarchy, bool isOnly, ref byte chunkIndex){
+                byte[] result = new byte[chunkIndex == 0 ? 5 : 2];
+
+                result[0] = chunkIndex;
+                if(chunkIndex == 0){
+                    result[1] = chunk;
+                    result[2] = hierarchy;
+                    result[3] = isOnly ? (byte)1 : (byte)0;
+                }
+                result[result.Length - 1] = 0;
+                chunkIndex++;
+
+                return result;
+            }
         }
     }
     public enum Reason {
