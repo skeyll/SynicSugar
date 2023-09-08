e491a2d80f6c6ba9bc12ea21a3cbc91e2113154f
Skeyll <studio.skeyll@gmail.com>
2023-09-08 09:15:16 +0900

    Add for matchmaking.

--- SynicSugar/Assets/SynicSugar/Runtime/MatchMake/LobbyEvent.cs
+++ SynicSugar/Assets/SynicSugar/Runtime/MatchMake/LobbyEvent.cs
@@ -29,38 +29,55 @@ namespace SynicSugar.MatchMake {
         /// To display text state.
         /// </summary>
         public Text stateText;
+        public bool canKick { get; internal set; }
     #region Event
-        public event Action EnableStart;
-        public event Action EnableCloseAndDisableStart;
+        /// <summary>
+        /// After push start button, prevent to be re-pressed matchmaking button.
+        /// </summary>
+        public event Action DisableStart;
+        /// <summary>
+        /// After join and create lobby, accept cancel matchmake and host's kick member.
+        /// </summary>
+        public event Action EnableCancelKick;
+        /// <summary>
+        /// This is enabled when the required members are passed to the Matchmaking API.<br />
+        /// After filled in the minimum number of members, can manually close Lobby to start p2p.
+        /// </summary>
+        public event Action EnableManualFinish;
+        /// <summary>
+        /// After complete or cancel matchmaking, prevent to change lobby state.
+        /// </summary>
+        public event Action DisableCancelKickFinish;
     #endregion
     #region Text
         /// <summary>
         /// Until join or create lobby.<br />
-        /// Stop to push StartMatchMake button.
+        /// Invoke DisableStart().
         /// </summary>
         public string StartMatchmaking;
         /// <summary>
-        /// Wait for opponents in Lobby until close Matchmake.<br />
-        /// Can kick member and cancel matchmake.
+        /// Wait for opponents in Lobby until finish Matchmake.<br />
+        /// Invoke EnableCancelKick(). canKick becomes true.
         /// </summary>
         public string WaitForOpponents;
         /// <summary>
-        /// After filld lobby, start to exchange data for p2p.<br />
-        /// Stop to kick and cancel.
+        /// After Filled lobby or close the matchmaking in manually, start to exchange data for p2p.<br />
+        /// Invoke DisableCancelKickFinish().
         /// </summary>
-        public string CloseMatchmaking;
+        public string FinishMatchmaking;
         /// <summary>
-        /// Can start p2p and game.
+        /// Can start p2p and game.<br />
+        /// (Move to GameScene or that UI mode.)
         /// </summary>
         public string ReadyForConnection;
         /// <summary>
         /// Leave or destroy lobby.<br />
-        /// Stop to kick and cancel.
+        /// Invoke DisableCancelKickFinish().
         /// </summary>
         public string TryToCancel;
     #endregion
         internal enum State {
-            Standby, Start, Wait, Close, Ready, Cancel
+            Standby, Start, Wait, Finish, Ready, Cancel
         }
         internal void ChangeState(State state){
             if(stateText == null){
@@ -76,8 +93,8 @@ namespace SynicSugar.MatchMake {
                 case State.Wait:
                     stateText.text = WaitForOpponents;
                 break;
-                case State.Close:
-                    stateText.text = CloseMatchmaking;
+                case State.Finish:
+                    stateText.text = FinishMatchmaking;
                 break;
                 case State.Ready:
                     stateText.text = ReadyForConnection;
