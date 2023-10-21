dbb4cfb889733fccb172405305a4329222c50a1d
Skeyll <studio.skeyll@gmail.com>
2023-10-21 15:18:52 +0900

    Large RPC change to multi thread.

--- SynicSugar/Assets/SynicSugar/Runtime/p2p/EOSp2p.cs
+++ SynicSugar/Assets/SynicSugar/Runtime/p2p/EOSp2p.cs
@@ -19,6 +19,7 @@ namespace SynicSugar.P2P {
             ArraySegment<byte> data = value is not null ? value : Array.Empty<byte>();
             ResultE result;
             int count = p2pConfig.Instance.RPCBatchSize;
+            
             foreach(var id in p2pInfo.Instance.userIds.RemoteUserIds){
                 SendPacketOptions options = new SendPacketOptions(){
                     LocalUserId = EOSManager.Instance.GetProductUserId(),
@@ -374,9 +375,12 @@ namespace SynicSugar.P2P {
         /// <param name="value"></param>
         /// <returns></returns>
         public async static UniTask SendLargePacketsToAll(byte ch, byte[] value){
+            await UniTask.SwitchToThreadPool();
+
             foreach(var id in p2pInfo.Instance.userIds.RemoteUserIds){
                 await SendLargePackets(ch, value, id);
             }
+            await UniTask.SwitchToMainThread();
         #if SYNICSUGAR_LOG
             Debug.Log($"Finish SendLargePacketsToAll for {ch}");
         #endif
@@ -401,9 +405,13 @@ namespace SynicSugar.P2P {
                 // ... = new byte[value.Length];
                 // Array.Copy(value, p2pInfo.Instance.lastRpcInfo.payload, value.Length);
             }
+            
+            await UniTask.SwitchToThreadPool();
+
             foreach(var id in p2pInfo.Instance.userIds.RemoteUserIds){
                 await SendLargePackets(ch, value, id);
             }
+            await UniTask.SwitchToMainThread();
         #if SYNICSUGAR_LOG
             Debug.Log($"Finish SendLargePacketsToAll for {ch}");
         #endif
