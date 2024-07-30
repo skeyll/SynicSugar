acbc793dcd3f156bc120ca350899043e3ec6fcdd
skeyll <skeyll@skeyll.net>
2024-07-30 15:02:51 +0900

    Fix the parent of object.

--- SynicSugar/Assets/SynicSugar/Samples~/GameSample/2_Tank/Scripts/Game/Tank/TankHealth.cs
+++ SynicSugar/Assets/SynicSugar/Samples~/GameSample/2_Tank/Scripts/Game/Tank/TankHealth.cs
@@ -20,7 +20,7 @@ namespace SynicSugar.Samples.Tank {
 
         private void Awake(){
             // Instantiate the explosion prefab and get a reference to the particle system on it.
-            m_ExplosionParticles = Instantiate(m_ExplosionPrefab).GetComponent<ParticleSystem>();
+            m_ExplosionParticles = Instantiate(m_ExplosionPrefab, this.transform).GetComponent<ParticleSystem>();
 
             // Get a reference to the audio source on the instantiated prefab.
             m_ExplosionAudio = m_ExplosionParticles.GetComponent<AudioSource>();
