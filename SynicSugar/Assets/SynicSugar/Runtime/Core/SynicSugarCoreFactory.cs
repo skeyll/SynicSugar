5ede0d31a3d7d4a0e9f0b846b0758d2b4cb06a5e
skeyll <skeyll@skeyll.net>
2024-08-22 14:50:16 +0900

    Fix: use EOSLobby instead of 2.

--- SynicSugar/Assets/SynicSugar/Runtime/Core/SynicSugarCoreFactory.cs
+++ SynicSugar/Assets/SynicSugar/Runtime/Core/SynicSugarCoreFactory.cs
@@ -2,7 +2,7 @@ using SynicSugar.MatchMake;
 namespace SynicSugar {
     internal sealed class SynicSugarCoreFactory : SynicSugarCoreFactoryBase {
         public override MatchmakingCore GenerateMatchmakingCore(uint maxSearch){
-            return new EOSLobby2(maxSearch);
+            return new EOSLobby(maxSearch);
         }
     }
 }
