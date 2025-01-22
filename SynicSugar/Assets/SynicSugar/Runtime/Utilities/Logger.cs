e40473560d08b6caf42fe705b7a5028e18fa56ac
skeyll <skeyll@skeyll.net>
2025-01-22 19:09:00 +0900

    Add: for usability.

--- SynicSugar/Assets/SynicSugar/Runtime/Utilities/Logger.cs
+++ SynicSugar/Assets/SynicSugar/Runtime/Utilities/Logger.cs
@@ -6,30 +6,43 @@ namespace SynicSugar
     internal class Logger
     {
         [Conditional("SYNICSUGAR_LOG")] 
-        public static void Log(string methodName, string message)
+        internal static void Log(string methodName, string message)
         {
             UnityEngine.Debug.Log($"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}] [INFO] {methodName}: {message}");
         }
         [Conditional("SYNICSUGAR_LOG")] 
-        public static void Log(string methodName, string message, Result result)
+        internal static void Log(string methodName, Result result)
+        {
+            UnityEngine.Debug.Log($"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}] [INFO] {methodName} Result: {result}");
+        }
+        [Conditional("SYNICSUGAR_LOG")] 
+        internal static void Log(string methodName, string message, Result result)
         {
             UnityEngine.Debug.Log($"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}] [INFO] {methodName}: {message} Result: {result}");
         }
 
-        public static void LogWarning(string methodName, string message)
+        internal static void LogWarning(string methodName, string message)
         {
             UnityEngine.Debug.LogWarning($"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}] [WARN] {methodName}: {message}");
         }
-        public static void LogWarning(string methodName, string message, Result result)
+        internal static void LogWarning(string methodName, Result result)
+        {
+            UnityEngine.Debug.LogWarning($"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}] [WARN] {methodName} Result: {result}");
+        }
+        internal static void LogWarning(string methodName, string message, Result result)
         {
             UnityEngine.Debug.LogWarning($"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}] [WARN] {methodName}: {message} Result: {result}");
         }
         
-        public static void LogError(string methodName, string message)
+        internal static void LogError(string methodName, string message)
         {
             UnityEngine.Debug.LogError($"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}] [ERROR] {methodName}: {message}");
         }
-        public static void LogError(string methodName, string message, Result result)
+        internal static void LogError(string methodName, Result result)
+        {
+            UnityEngine.Debug.LogError($"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}] [ERROR] {methodName} Result: {result}");
+        }
+        internal static void LogError(string methodName, string message, Result result)
         {
             UnityEngine.Debug.LogError($"[{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}] [ERROR] {methodName}: {message} Result: {result}");
         }
