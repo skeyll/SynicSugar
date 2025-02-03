ffb5c46ac6b4eb205cb8ee3586d818d87ab45c16
skeyll <skeyll@skeyll.net>
2025-02-03 17:41:41 +0900

    Add: the comments.

--- SynicSugar/Assets/SynicSugar/Runtime/RTC/VCMode.cs
+++ SynicSugar/Assets/SynicSugar/Runtime/RTC/VCMode.cs
@@ -1,6 +1,13 @@
 namespace SynicSugar.RTC {
     public enum VCMode 
     {
-        OpenVC, PushToTalk
+        /// <summary>
+        /// OpenVC toggles between on and off. When on, voice communication is automatically sent, and when off, the voice communication is stopped.
+        /// </summary>
+        OpenVC, 
+        /// <summary>
+        /// PushToTalk sends voice communication only while the specified key is held down. When the key is released, voice communication stops."
+        /// </summary>
+        PushToTalk
     }
 }
